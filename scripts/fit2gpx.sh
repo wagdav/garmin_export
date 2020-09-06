096a6ad822f03a3196b50b7f2b3da893a2a9af82
David Wagner <wagdav@gmail.com>
2020-09-06 17:00:11 +0200

    Add fit2gpx.sh script

---
+++ scripts/fit2gpx.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -p gpsbabel -i bash
+# shellcheck shell=bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: fit2gpx.sh <FIT_FILE>"
+fi
+
+fit_file=$1
+gpsbabel -i garmin_fit -f "$fit_file" -o gpx -F "${fit_file%.*}.gpx"
